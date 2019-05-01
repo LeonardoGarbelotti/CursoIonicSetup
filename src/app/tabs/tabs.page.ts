43899cb728af4d6c92f4af2343e94eba9b24e059
Leonardo G <leo13garbelotti@hotmail.com>
2019-05-01 17:27:56 -0300

    primeiro commit

---
+++ src/app/tabs/tabs.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RegistrarPage } from '../registrar/registrar.page';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss']
+})
+export class TabsPage {
+
+  
+}
